c1f20656c3c3b8c1323c21b8c282188cef9fdb47
Luke Visinoni <luke.visinoni@gmail.com>
2016-12-25 04:23:39 -0800

    Fixed a few styleci related things

--- src/Collection/AbstractCollection.php
+++ src/Collection/AbstractCollection.php
@@ -3,7 +3,6 @@
  * Nozavroni/Collections
  * Just another collections library for PHP5.6+.
  *
- * @version   {version}
  * @copyright Copyright (c) 2016 Luke Visinoni <luke.visinoni@gmail.com>
  * @author    Luke Visinoni <luke.visinoni@gmail.com>
  * @license   https://github.com/deni-zen/csvelte/blob/master/LICENSE The MIT License (MIT)
